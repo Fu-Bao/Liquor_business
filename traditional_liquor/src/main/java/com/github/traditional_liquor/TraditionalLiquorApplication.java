79065f6a57346eb15b2fb8c4990125bb8eca6912
YDongGeun <97018542+YDongGeun@users.noreply.github.com>
2024-04-10 03:19:36 +0900

    .

---
+++ traditional_liquor/src/main/java/com/github/traditional_liquor/TraditionalLiquorApplication.java
@@ -0,0 +1,13 @@
+package com.github.traditional_liquor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraditionalLiquorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraditionalLiquorApplication.class, args);
+    }
+
+}
